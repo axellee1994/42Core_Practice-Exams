d4266e1e5e73a9c1e3ef5c936be51da4ce23e705
Lee Zhong Yu Axel <124663345+axellee1994@users.noreply.github.com>
2024-02-25 10:48:33 +0800

    Update expand_str.c

--- Exam Rank 02/Level 03/expand_str/expand_str.c
+++ Exam Rank 02/Level 03/expand_str/expand_str.c
@@ -1,33 +1,29 @@
 #include <unistd.h>
-#include <stdio.h>
 
 int main(int argc, char **argv)
 {
-    int i;
-    int space;
-
-    i = 0;
-    space = 1;
-    if (argc == 2)
-    {
-        while (argv[1][i])
-        {
-            if (argv[1][i] == 32 || (argv[1][i] >= 9 && argv[1][i] <= 13))
-            {
-                if (!space && argv[1][i+1] != '\0')
-                {
-                    write(1, "   ", 3);
-                    space = 1;
-                }
-            }
-            else
-            {
-                write(1, &argv[1][i], 1);
-                space = 0;
-            }
-            i++;
-        }
-    }
-    write(1, "\n", 1);
-    return (0);
+	int i;
+	int flag;
+	
+	i = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i] == 32 || argv[1][i] == 9)
+			i++;
+		while (argv[1][i])
+		{
+			if (argv[1][i] == 32 || argv[1][i] == 9)
+				flag = 1;
+			if (!(argv[1][i] == 32 && argv[1][i] == 9))
+			{
+				if (flag)
+					write(1, "   ", 3);
+				flag = 0;
+				write(1, &argv[1][i], 1);
+			}
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
 }
