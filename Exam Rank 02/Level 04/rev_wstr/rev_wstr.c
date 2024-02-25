8a5e93fbf1130fe95bd5b3e2615b002d24169d99
Lee Zhong Yu Axel <124663345+axellee1994@users.noreply.github.com>
2024-02-25 10:46:04 +0800

    Update rev_wstr.c

--- Exam Rank 02/Level 04/rev_wstr/rev_wstr.c
+++ Exam Rank 02/Level 04/rev_wstr/rev_wstr.c
@@ -1,51 +1,45 @@
 #include <unistd.h>
+#include <stdio.h>
 
-int is_space(char c)
+void	rev_wstr(char *str)
 {
-    if (c == 32 || c == 9 || c == 10)
-        return (1);
-    return (0);
+	int	i;
+	int	word_start;
+	int	word_end;
+
+	i = 0;
+	while (str[i])
+		i++;
+	i--;
+	while (i >= 0)
+	{
+		while (str[i] && (str[i] == 32 || str[i] == 9))
+			i--;
+		word_end = i;
+		while (str[i] && (str[i] != 32 && str[i] != 9))
+			i--;
+		word_start = i + 1;
+		while (word_start <= word_end)
+		{
+			write(1, &str[word_start], 1);
+			word_start++;
+		}
+		if (i > 0)
+			write(1, " ", 1);
+	}
 }
 
-void    rev_wstr(char *str)
+int	main(int argc, char **argv)
 {
-    int i;
-    int word_start;
-    int word_end;
-
-    i = 0;
-    while (str[i])
-        i++;
-    i--;
-    while (i >= 0)
-    {
-        while(i >= 0 && is_space(str[i]))
-            i--;
-        word_end = i;
-        while (i >= 0 && !is_space(str[i]))
-            i--;
-        word_start = i + 1;
-        while (word_start <= word_end)
-        {
-            write(1, &str[word_start], 1);
-            word_start++;
-        }
-        if (i >= 0)
-            write(1, " ", 1);
-    }
-}
-
-int main(int argc, char **argv)
-{
-    if (argc < 2)
-    {
-        write(1, "\n", 1);
-        return (0);
-    }
-    else
-    {
-        rev_wstr(argv[1]);
-        write(1, "\n", 1);
-        return (0);
-    }
+	if (argc < 2)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	else
+	{
+		rev_wstr(argv[1]);
+		write(1, "\n", 1);
+		return (0);
+	}
 }
