8b893e7fb6eb03d34f36d612bd2a7b9d9d041327
Axel Lee <axellee1994@gmail.com>
2024-03-01 21:22:05 +0800

    GNL Solution which is fully formatted

---
+++ Exam Rank 03/get_next_line/get_next_line.c
@@ -0,0 +1,106 @@
+#include "get_next_line.h"
+
+char	*ft_strchar(char *str, int c)
+{
+	while (*str)
+	{
+		if (*str == (char)c)
+			return ((char *)str);
+		str++;
+	}
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dst_size)
+{
+	size_t	src_size;
+	size_t	i;
+
+	src_size = 0;
+	i = 0;
+	src_size = ft_strlen(src);
+	if (dst_size > 0)
+	{
+		while (i < src_size && i < dst_size - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (src_size);
+}
+
+char	*ft_strdup(const char *src)
+{
+	char	*dst;
+	size_t	len;
+
+	len = ft_strlen(src) + 1;
+	dst = malloc(len);
+	if (dst == NULL)
+		return (NULL);
+	ft_strlcpy(dst, src, len);
+	return (dst);
+}
+
+char	*ft_strjoin(char *s1, const char *s2, size_t len)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*join;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = len;
+	join = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (!join)
+		return (NULL);
+	ft_strlcpy(join, s1, s1_len + 1);
+	ft_strlcpy((join + s1_len), s2, s2_len + 1);
+	free(s1);
+	return (join);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buf[BUFFER_SIZE + 1];
+	char		*line;
+	char		*newline;
+	int			countread;
+	int			to_copy;
+
+	line = ft_strdup(buf);
+	while (!(ft_strchar(line, '\n')) && (countread = read(fd, buf,
+				BUFFER_SIZE)) > 0)
+	{
+		buf[countread] = '\0';
+		line = ft_strjoin(line, buf, countread);
+	}
+	if (ft_strlen(line) == 0)
+		return (free(line), NULL);
+	newline = ft_strchar(line, '\n');
+	if (newline != NULL)
+	{
+		to_copy = newline - line + 1;
+		ft_strlcpy(buf, newline + 1, BUFFER_SIZE + 1);
+	}
+	else
+	{
+		to_copy = ft_strlen(line);
+		ft_strlcpy(buf, "", BUFFER_SIZE + 1);
+	}
+	line[to_copy] = '\0';
+	return (line);
+}
