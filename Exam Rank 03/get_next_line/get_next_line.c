bbbf04ade1bccf4c294bd0b6d13460b13833bb0c
Lee Zhong Yu Axel <124663345+axellee1994@users.noreply.github.com>
2024-04-25 15:16:54 +0800

    New GNL Solution

--- Exam Rank 03/get_next_line/get_next_line.c
+++ Exam Rank 03/get_next_line/get_next_line.c
@@ -1,122 +1,44 @@
 #include "get_next_line.h"
 
-char	*ft_strchar(char *str, int c)
+char *get_next_line(int fd)
 {
-	while (*str)
+	static char buff[9999999] = {0};
+	static int i = 0;
+	int k = 0, read_byte = 1;
+	char *line;
+
+	while (read_byte > 0)
 	{
-		if (*str == (char)c)
-			return ((char *)str);
-		str++;
+		read_byte = read(fd, &buff[k], BUFFER_SIZE);
+		if (read_byte == -1 || buff[i] == '\0')
+			return NULL;
+		k += read_byte;
 	}
-	return (NULL);
+	read_byte = 0;
+	while (buff[i + read_byte] && buff[i + read_byte] != '\n')
+		read_byte++;
+	line = malloc(read_byte + 1 + (buff[i + read_byte] == '\n'));
+	if (!line)
+		return NULL;
+	line[read_byte + (buff[i + read_byte] == '\n')] = '\0';
+	k = 0;
+	while (k <= read_byte)
+		line[k++] = buff[i++];	
+	return line;
 }
 
-size_t	ft_strlen(const char *str)
+int	main(void)
 {
-	size_t	i;
+	char	*line;
+	int	fd;
 
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t dst_size)
-{
-	size_t	src_size;
-	size_t	i;
-
-	src_size = 0;
-	i = 0;
-	src_size = ft_strlen(src);
-	if (dst_size > 0)
-	{
-		while (i < src_size && i < dst_size - 1)
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
-	}
-	return (src_size);
-}
-
-char	*ft_strdup(const char *src)
-{
-	char	*dst;
-	size_t	len;
-
-	len = ft_strlen(src) + 1;
-	dst = malloc(len);
-	if (dst == NULL)
-		return (NULL);
-	ft_strlcpy(dst, src, len);
-	return (dst);
-}
-
-char	*ft_strjoin(char *s1, const char *s2, size_t len)
-{
-	size_t	s1_len;
-	size_t	s2_len;
-	char	*join;
-
-	if (!s1 || !s2)
-		return (NULL);
-	s1_len = ft_strlen(s1);
-	s2_len = len;
-	join = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
-	if (!join)
-		return (NULL);
-	ft_strlcpy(join, s1, s1_len + 1);
-	ft_strlcpy((join + s1_len), s2, s2_len + 1);
-	free(s1);
-	return (join);
-}
-
-char	*get_next_line(int fd)
-{
-	static char	buf[BUFFER_SIZE + 1];
-	char		*line;
-	char		*newline;
-	int			countread;
-	int			to_copy;
-
-	line = ft_strdup(buf);
-	while (!(ft_strchar(line, '\n')) && (countread = read(fd, buf,
-				BUFFER_SIZE)) > 0)
-	{
-		buf[countread] = '\0';
-		line = ft_strjoin(line, buf, countread);
-	}
-	if (ft_strlen(line) == 0)
-		return (free(line), NULL);
-	newline = ft_strchar(line, '\n');
-	if (newline != NULL)
-	{
-		to_copy = newline - line + 1;
-		ft_strlcpy(buf, newline + 1, BUFFER_SIZE + 1);
-	}
-	else
-	{
-		to_copy = ft_strlen(line);
-		ft_strlcpy(buf, "", BUFFER_SIZE + 1);
-	}
-	line[to_copy] = '\0';
-	return (line);
-}
-
-int main(void)
-{
-    int fd;
-    char    *line;
-
-    fd = open("test.txt", O_RDONLY);
-    line = get_next_line(fd);
-    while (line  != NULL)
-    {
-        printf("%s\n", line);
-        free(line);
-    }
-    close(fd);
-    return (0);
+	fd = open("test",O_RDONLY);
+	line = get_next_line(fd);
+	printf("%s", line);
+	line = get_next_line(fd);
+	printf("%s", line);
+	line = get_next_line(fd);
+	printf("%s", line);
+	line = get_next_line(fd);
+	printf("%s", line);
 }
